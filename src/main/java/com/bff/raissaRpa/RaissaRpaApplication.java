292de1fccabf1e7334299a3cf656b5d9db1b41ab
Gamma Pro <adm.gammapro@gmail.com>
2025-10-27 10:12:03 -0500

    primera carga

---
+++ src/main/java/com/bff/raissaRpa/RaissaRpaApplication.java
@@ -0,0 +1,13 @@
+package com.bff.raissaRpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaissaRpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaissaRpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bff/raissaRpa/RaissaRpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bff.raissaRpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaissaRpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
